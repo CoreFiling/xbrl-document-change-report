224f97c3022c5b2b4cac1c18d9e37320c5129427
Damian Cugley <pdc@corefiling.com>
2017-11-21 16:12:15 +0000

    Fake diff data for story.

--- src/app/components/table.stories.tsx
+++ src/app/components/table.stories.tsx
@@ -17,41 +17,116 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { lShapedTableWithMetadata } from '@cfl/table-viewer/lib/test-utils/l-shaped-headers';
+import { TableChunk, Fact } from '@cfl/table-rendering-service';
+import { DiffTableChunk, DiffFact, DiffCell, FactMappingSourceEnum } from '@cfl/table-diff-service';
 
 import QueryableTablePageImpl from '../models/queryable-table-page-impl';
 import Table from './table';
 
+type DiffStatus = 'NOP' | 'ADD' | 'DEL' | 'CHG';
+const DIFF_STATUSES: DiffStatus[] = ['NOP', 'ADD', 'DEL', 'CHG'];
+
+function getDiffStatus(x: number, y: number, z: number): DiffStatus {
+  return DIFF_STATUSES[(x + y + z) % 4];
+}
+
+/**
+ * Given a normal table chunk, return one representing a comparison table.
+ */
+function mungeTableChunk(chunk: TableChunk): TableChunk {
+  const { z, data } = chunk;
+  let nextId = Math.max(...data.map(col => Math.max(...col.map(cell => cell ? Math.max(...cell.facts.map(fact => fact.id)) : -1))));
+  function getId(): number {
+    return ++nextId;
+  }
+  return {
+    ...chunk,
+    data: data.map((col, x) => col.map((cell, y) => {
+      const diffStatus = getDiffStatus(x, y, z);
+      let facts: Fact[];
+      const issues: number[] = cell ? cell.issues : [];
+      if (diffStatus === 'NOP') {
+        return cell;
+      } else if (!cell) {
+        facts = [{stringValue: '42', id: getId()}];
+      } else {
+        facts = cell.facts;
+      }
+      if (diffStatus === 'CHG') {
+        const newFacts = [...facts, ...facts.map(() => ({stringValue: '69', id: getId()}))];
+        return { issues, facts: newFacts };
+      }
+      return { issues, facts };
+    })),
+  };
+}
+
+function makeDiffChunk(chunk: TableChunk): DiffTableChunk {
+  const { x, y, z, data } = chunk;
+  let nextId = 1000;
+  function getId(): number {
+    return ++nextId;
+  }
+  return {
+    x, y, z,
+    data: data.map((col, dx) => col.map((cell, dy) => {
+      const diffStatus = getDiffStatus(dx, dy, z);
+      let facts: DiffFact[];
+      switch (diffStatus) {
+        case 'NOP':
+          if (cell) {
+            facts = cell.facts.map(f => ({
+              diffStatus,
+              from: {id: getId(), sourceId: f.id, source: 'FROM' as FactMappingSourceEnum},
+              to: {id: getId(), sourceId: f.id, source: 'TO' as FactMappingSourceEnum},
+            }));
+          } else {
+            facts = [];
+          }
+          break;
+        case 'ADD':
+          facts = cell.facts.map(f => ({
+            diffStatus,
+            to: {id: getId(), sourceId: f.id, source: 'TO' as FactMappingSourceEnum},
+          }));
+          break;
+        case 'ADD':
+          facts = cell.facts.map(f => ({
+            diffStatus,
+            from: {id: getId(), sourceId: f.id, source: 'FROM' as FactMappingSourceEnum},
+          }));
+          break;
+        case 'CHG':
+          {
+            const fs = cell.facts;
+            const diffFactCount = fs.length / 2;
+            facts = cell.facts.slice(0, diffFactCount).map((f1, j) => {
+              const f2 = fs[diffFactCount + j];
+              return {
+                diffStatus,
+                from: {id: getId(), sourceId: f1.id, source: 'FROM' as FactMappingSourceEnum},
+                to: {id: getId(), sourceId: f2.id, source: 'TO' as FactMappingSourceEnum},
+              };
+            });
+          }
+          break;
+        default:
+          // Should not happen.
+          facts = [];
+          break;
+      }
+      const diffCell: DiffCell = { diffStatus, facts };
+      return diffCell;
+    })),
+  };
+}
+
 storiesOf('Table', module)
-  .add('Simple pass', () => {
-    const { table, metadata, zOptions} = lShapedTableWithMetadata();
-
-    return (
-      <Table
-        metadata={metadata}
-        zOptions={zOptions}
-        table={table}
-        onChangePage={action('onChangePage')}
-        onChangeTable={action('onChangeTable')}
-      />
-    );
-  })
-  .add('Simple fail', () => {
-    const { table, metadata, zOptions} = lShapedTableWithMetadata();
-
-    return (
-      <Table
-        metadata={metadata}
-        zOptions={zOptions}
-        table={table}
-        onChangePage={action('onChangePage')}
-        onChangeTable={action('onChangeTable')}
-      />
-    );
-  })
-  .add('Many Z options', () => {
+  .add('Semi-real diff table', () => {
     const { tables, zOptions, tableChunk } = require('../../stories/table-a.json');
-    const table = new QueryableTablePageImpl(tables[0], tableChunk);
+    const mungedTableChunk = mungeTableChunk(tableChunk);
+    const diffChunk = makeDiffChunk(mungedTableChunk);
+    const table = new QueryableTablePageImpl(tables[0], mungedTableChunk, diffChunk);
     return (
       <Table
         metadata={tables[0]}
